c8abf0d88a0da5f1ce130fb4fbcde5306191a7d1
Scott Jehl <scott@sj.local>
2012-07-06 16:06:40 -0500

    moved unit tests

--- grunt.js
+++ grunt.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       }
     },
     qunit: {
-      files: ['test/**/*.html']
+      files: ['test/unit/**/*.html']
     },
     lint: {
       files: ['grunt.js', 'src/*.js', 'test/**/*.js']
