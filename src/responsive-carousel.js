4ac2eb3c5960fef6ae17bba0d3f7de58e18bd8fd
Scott Jehl <scott@scottjehl.com>
2013-03-12 12:37:20 -0500

    updated dist files

--- src/responsive-carousel.js
+++ src/responsive-carousel.js
@@ -7,7 +7,7 @@
  */
 
 (function($) {
-	
+
 	var pluginName = "carousel",
 		initSelector = "." + pluginName,
 		transitionAttr = "data-transition",
@@ -40,17 +40,17 @@
 					[ pluginName ]( "_addNextPrev" )
 					.trigger( "create." + pluginName );
 			},
-			
+
 			_init: function(){
 				var trans = $( this ).attr( transitionAttr );
-				
+
 				if( !trans ){
 					cssTransitionsSupport = false;
 				}
-				
+
 				return $( this )
 					.addClass(
-						pluginName + 
+						pluginName +
 						" " + ( trans ? pluginName + "-" + trans : "" ) + " "
 					)
 					.children()
@@ -58,31 +58,31 @@
 					.first()
 					.addClass( activeClass );
 			},
-			
+
 			next: function(){
 				$( this )[ pluginName ]( "goTo", "+1" );
 			},
-			
+
 			prev: function(){
 				$( this )[ pluginName ]( "goTo", "-1" );
 			},
-			
+
 			goTo: function( num ){
-				
+
 				var $self = $(this),
 					trans = $self.attr( transitionAttr ),
 					reverseClass = " " + pluginName + "-" + trans + "-reverse";
-				
+
 				// clean up children
 				$( this ).find( "." + itemClass ).removeClass( [ outClass, inClass, reverseClass ].join( " " ) );
-				
+
 				var $from = $( this ).find( "." + activeClass ),
 					prevs = $from.index(),
 					activeNum = ( prevs < 0 ? 0 : prevs ) + 1,
 					nextNum = typeof( num ) === "number" ? num : activeNum + parseFloat(num),
 					$to = $( this ).find( ".carousel-item" ).eq( nextNum - 1 ),
 					reverse = ( typeof( num ) === "string" && !(parseFloat(num)) ) || nextNum > activeNum ? "" : reverseClass;
-				
+
 				if( !$to.length ){
 					$to = $( this ).find( "." + itemClass )[ reverse.length ? "last" : "first" ]();
 				}
@@ -97,31 +97,31 @@
 				// added to allow pagination to track
 				$self.trigger( "goto." + pluginName, $to );
 			},
-			
+
 			update: function(){
 				$(this).children().not( "." + navClass ).addClass( itemClass );
-				
+
 				return $(this).trigger( "update." + pluginName );
 			},
-			
+
 			_transitionStart: function( $from, $to, reverseClass ){
 				var $self = $(this);
-				
+
 				$to.one( navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ? "webkitTransitionEnd" : "transitionend otransitionend", function(){
 					$self[ pluginName ]( "_transitionEnd", $from, $to, reverseClass );
 				});
-				
+
 				$(this).addClass( reverseClass );
 				$from.addClass( outClass );
-				$to.addClass( inClass );	
+				$to.addClass( inClass );
 			},
-			
+
 			_transitionEnd: function( $from, $to, reverseClass ){
-				$(this).removeClass( reverseClass );
+				$( this ).removeClass( reverseClass );
 				$from.removeClass( outClass + " " + activeClass );
 				$to.removeClass( inClass ).addClass( activeClass );
 			},
-						
+
 			_bindEventListeners: function(){
 				var $elem = $( this )
 					.bind( "click", function( e ){
@@ -131,21 +131,21 @@
 							e.preventDefault();
 						}
 					});
-				
+
 				return this;
 			},
-			
+
 			_addNextPrev: function(){
 				return $( this )
 					.append( "<nav class='"+ navClass +"'><a href='#prev' class='prev' aria-hidden='true' title='Previous'>Prev</a><a href='#next' class='next' aria-hidden='true' title='Next'>Next</a></nav>" )
 					[ pluginName ]( "_bindEventListeners" );
 			},
-			
+
 			destroy: function(){
 				// TODO
 			}
 		};
-		
+
 	// Collection method.
 	$.fn[ pluginName ] = function( arrg, a, b, c ) {
 		return this.each(function() {
@@ -154,12 +154,12 @@
 			if( arrg && typeof( arrg ) === "string" ){
 				return $.fn[ pluginName ].prototype[ arrg ].call( this, a, b, c );
 			}
-			
+
 			// don't re-init
 			if( $( this ).data( pluginName + "data" ) ){
 				return $( this );
 			}
-			
+
 			// otherwise, init
 			$( this ).data( pluginName + "active", true );
 			$.fn[ pluginName ].prototype._create.call( this );
