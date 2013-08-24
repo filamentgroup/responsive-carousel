ef39f1d33b098261c7a3087ac4ae3cb86f505239
John Bender <john.m.bender@gmail.com>
2013-08-24 13:15:14 -0700

    use early return to clean up the bound functions

--- src/responsive-carousel.loop.js
+++ src/responsive-carousel.loop.js
@@ -14,57 +14,58 @@
 
 	$.extend( $.fn[pluginName].prototype, {
 		_loopInit: function() {
-			var $this = $(this);
+			var $this = $(this),
+				prototype = $.fn[pluginName].prototype;
 
-			$this.on( "beforegoto." + pluginName,
-				$.proxy($.fn[pluginName].prototype._loopBeforeGoto, this));
-
-			$this.on( "beforecreatenav." + pluginName,
-				$.proxy($.fn[pluginName].prototype._loopBeforeCreateNav, this));
+			$this.on( "beforegoto." + pluginName, prototype._loopBeforeGoto);
+			$this.on( "beforecreatenav." + pluginName, prototype._loopBeforeCreateNav);
 		},
 
 		_loopBeforeGoto: function( event, data ) {
 			var $this = $(event.target), index = data.nextIndex, items = data.items;
 
-			if( !isLooped( $this ) ) {
-				// if the request index is larger than the set of carousel items or smaller than zero
-				// and the carousel has been anotated correctly, prevent wrapping
-				if( (index < 0 || index > (items.length - 1))) {
-					event.preventDefault();
-					return;
-				}
+			// if this carousel is supposed to loop, skip
+			if( isLooped( $this ) ) {
+				return;
+			}
 
-				if( index === items.length - 1 ){
-					$this[pluginName]( '_disableNav', 'next' );
-				}
+			// if the request index is greater than the # of items or smaller than zero
+			if( (index < 0 || index > (items.length - 1))) {
+				event.preventDefault();
+				return;
+			}
 
-				if( index === 0 ){
-					$this[pluginName]( '_disableNav', 'prev' );
-				}
+			if( index === items.length - 1 ){
+				$this[pluginName]( '_disableNav', 'next' );
+			}
 
-				if( index > 0 && index < items.length - 1 ) {
-					$this[pluginName]( '_enableNav', 'next' );
-					$this[pluginName]( '_enableNav', 'prev' );
-				}
+			if( index === 0 ){
+				$this[pluginName]( '_disableNav', 'prev' );
+			}
+
+			if( index > 0 && index < items.length - 1 ) {
+				$this[pluginName]( '_enableNav', 'next' );
+				$this[pluginName]( '_enableNav', 'prev' );
 			}
 		},
 
 		_loopBeforeCreateNav: function( event, data ) {
 			var $this = $(event.target), $items, $active;
 
-			$items = $this.find( "." + itemClass );
+			if( isLooped( $this ) ) {
+				return;
+			}
 
+			$items = $this.find( "." + itemClass );
 			$active = $items.filter( "." + activeClass );
 
 			// if this is not a looped carousel enable and disable nav appropriately
-			if( !isLooped( $this ) ) {
-				if( $active[0] === $items[0]) {
-					$this[pluginName]( '_disableNav', 'prev', data.$nav );
-				}
+			if( $active[0] === $items[0]) {
+				$this[pluginName]( '_disableNav', 'prev', data.$nav );
+			}
 
-				if( $active.last()[0] === $items.last()[0]) {
-					$this[pluginName]( '_disableNav', 'next', data.$nav );
-				}
+			if( $active.last()[0] === $items.last()[0]) {
+				$this[pluginName]( '_disableNav', 'next', data.$nav );
 			}
 		},
 
