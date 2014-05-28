554a193acb2c653710bb9d5f36456c103ca45ba5
Scott Jehl <scottjehl@gmail.com>
2014-05-28 16:12:38 -0500

    more compatible approach to ajax-include usage

--- src/responsive-carousel.ajax.js
+++ src/responsive-carousel.ajax.js
@@ -7,15 +7,15 @@
  */
 
 (function($) {
-	
+
 	var pluginName = "carousel",
 		initSelector = "." + pluginName;
-	
+
 	// DOM-ready auto-init
-	$( document ).on( "ajaxInclude", initSelector, function(){
-		$( this )[ pluginName ]( "update" );
+	$( document ).bind( "ajaxInclude", function( e ){
+		$( e.target ).closest( initSelector )[ pluginName ]( "update" );
 	} );
-	
+
 	// kick off ajaxIncs at dom ready
 	$( function(){
 		$( "[data-after],[data-before]", initSelector ).ajaxInclude();
